5516dc0e433bcbbdf291dd0278180e7592c3e544
Clément Di Domenico <clement.didom@gmail.com>
2024-09-17 10:51:42 +0200

    feat: setup springboot app

---
+++ server/src/main/java/com/solona/demo_auth_aut/Application.java
@@ -0,0 +1,13 @@
+package com.solona.demo_auth_aut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
